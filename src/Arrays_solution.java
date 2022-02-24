ee3387a917a19bbb95fa809bd310426acdd37366
Barclaysdev2011 <shakilmustafiz17@gmail.com>
2022-02-23 22:06:16 -0500

    Addded class and method

---
+++ src/Arrays_solution.java
@@ -0,0 +1,20 @@
+public class Arrays_solution {
+    public static void main(String[]args){
+        /*
+        @author Shakil
+
+         */
+
+
+    }
+
+
+    /*
+       1.1  Is unique: Implement an algorithm to determine if a string has all unique characters.
+             what if you cant use additional data structure?
+      */
+    static boolean isUnique(){
+
+        return true;
+    }
+}
