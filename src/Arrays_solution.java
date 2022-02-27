2ae91e46af4d93b5072b156f6227cf428b502cb0
Barclaysdev2011 <shakilmustafiz17@gmail.com>
2022-02-27 13:45:58 -0500

    Given an array of integers nums and an integer target

--- src/Arrays_solution.java
+++ src/Arrays_solution.java
@@ -1,11 +1,20 @@
+
+import java.util.HashMap;
+
+
 public class Arrays_solution {
-    public static void main(String[]args){
+    public static void main(String[]args) {
         /*
         @author Shakil
 
          */
 
-        System.out.println(isUnique("avocado"));
+      System.out.println(isUnique("avocado"));
+
+        int[] numbs = {3,2,4}; int target = 9;
+        twoSum(numbs,target);
+
+
     }
 
 
@@ -24,4 +33,26 @@ public class Arrays_solution {
         }
         return true;
     }
+    /*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+     */
+
+   static int [] twoSum(int [] nums ,int target){
+       HashMap<Integer, Integer> seen = new HashMap<>();
+       int[] res = new int[2];
+
+       for (int i = 0; i < nums.length; i++) {
+           if (seen.containsKey(target - nums[i])) {
+               res[0] = seen.get(target - nums[i]);
+               res[1] = i;
+               break;
+           }
+           seen.put(nums[i], i);
+       }
+       return res;
+    }
+
 }
